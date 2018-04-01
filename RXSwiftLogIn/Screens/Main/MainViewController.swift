a549e8be203019f700d92192fe15372b8a88bd94
Damian Markowski <contact@damianmarkowski.pl>
2018-04-01 14:20:47 +0200

    views added to main screen

--- RXSwiftLogIn/Screens/Main/MainViewController.swift
+++ RXSwiftLogIn/Screens/Main/MainViewController.swift
@@ -6,16 +6,28 @@
 //  Copyright (c) 2018 Damian Markowski. All rights reserved.
 
 import UIKit
+import SnapKit
 
 protocol MainDisplayLogic: class {
   
 }
 
 class MainViewController: UIViewController, MainDisplayLogic {
-  
+    
+    // Clean Swift Architecture layers
     var interactor: MainBusinessLogic?
     var router: (NSObjectProtocol & MainRoutingLogic)?
-
+    // Views
+    private var emailTextField: UITextField!
+    private var passwordTextField: UITextField!
+    private var logInButton: UIButton!
+    // Constants
+    private let viewsHorizontalMargin = 30
+    private let viewsVerticalMargin = 20
+    private let emailTextFieldPlaceholder = "E-mail"
+    private let passwordTextFieldPlaceholder = "Password"
+    private let logInButtonTitle = "Log in"
+    
     // MARK: Setup
   
     private func setup() {
@@ -39,5 +51,81 @@ class MainViewController: UIViewController, MainDisplayLogic {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addViews()
+    }
+    
+    // MARK: Private methods
+    
+    private func addViews(){
+        addEmailTextField()
+        addPasswordTextField()
+        addLogInButton()
+    }
+    
+    private func addEmailTextField(){
+        emailTextField = UITextField()
+        configureEmailTextField()
+        view.addSubview(emailTextField)
+        addConstraintsToEmailTextField()
+    }
+    
+    private func configureEmailTextField(){
+        emailTextField.placeholder = emailTextFieldPlaceholder
+        configureTextFieldCommon(emailTextField)
+    }
+    
+    private func configureTextFieldCommon(_ textField: UITextField){
+        textField.autocapitalizationType = .none
+        textField.layer.borderWidth = 1
+    }
+    
+    private func addConstraintsToEmailTextField(){
+        emailTextField.snp.makeConstraints { make in
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(viewsHorizontalMargin)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(viewsVerticalMargin)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).offset(-viewsHorizontalMargin)
+        }
+    }
+    
+    private func addPasswordTextField(){
+        passwordTextField = UITextField()
+        configurePasswordTextField()
+        view.addSubview(passwordTextField)
+        addConstraintsToPasswordTextField()
+    }
+    
+    private func configurePasswordTextField(){
+        passwordTextField.placeholder = passwordTextFieldPlaceholder
+        passwordTextField.isSecureTextEntry = true
+        configureTextFieldCommon(passwordTextField)
+    }
+    
+    private func addConstraintsToPasswordTextField(){
+        passwordTextField.snp.makeConstraints { make in
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(viewsHorizontalMargin)
+            make.top.equalTo(emailTextField.snp.bottom).offset(viewsVerticalMargin)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).offset(-viewsHorizontalMargin)
+        }
+    }
+    
+    private func addLogInButton(){
+        logInButton = UIButton()
+        configureLogInButton()
+        view.addSubview(logInButton)
+        addConstraintsToLogInButton()
+    }
+    
+    private func configureLogInButton(){
+        logInButton.setTitle(logInButtonTitle, for: .normal)
+        logInButton.setTitleColor(UIColor.black, for: .normal)
+        logInButton.backgroundColor = UIColor.green
+    }
+    
+    private func addConstraintsToLogInButton(){
+        logInButton.snp.makeConstraints { make in
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(viewsHorizontalMargin)
+            make.top.equalTo(passwordTextField.snp.bottom).offset(viewsVerticalMargin)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).offset(-viewsHorizontalMargin)
+        }
     }
 }
